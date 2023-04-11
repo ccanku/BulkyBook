0f93a3eaae793998b5559daba6f3284a4bb910a4
ccanku <ccanku37@icloud.com>
2023-04-11 17:12:44 +0300

    Created UserController in Admin Area and according view which displays User List.

---
+++ BulkyBookWeb/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,61 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Bulky.Models.ViewModels;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles =SD.Role_Admin)]
+    public class UserController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> userList = _db.ApplicationUsers.Include(u=>u.Company).ToList();
+
+            var userRoles = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+
+            foreach(var user in userList)
+            {
+                var roleId = userRoles.FirstOrDefault(u => u.UserId == user.Id).RoleId;
+                user.Role = roles.FirstOrDefault(u=>u.Id == roleId).Name ;
+                if(user.Company == null)
+                {
+                    user.Company = new Company()
+                    {
+                        Name = ""
+                    };
+                }
+            }
+
+            return Json(new { data = userList });
+
+        }
+
+        
+        #endregion
+    }
+}
